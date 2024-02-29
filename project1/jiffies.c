99d06170c82de0dadfc323cc8f3372da178e9132
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-02-29 11:46:38 +0800

    Update jiffies.c

--- project1/jiffies.c
+++ project1/jiffies.c
@@ -8,9 +8,6 @@
 # define BUFFER_SIZE 128
 # define PROC_NAME "jiffies"
 
-/*
- * Function prototypes
- */
 static ssize_t proc_read(struct file* file, char* buffer, size_t cnt, loff_t* pos);
 
 static struct file_operations proc_ops = {
@@ -18,9 +15,38 @@ static struct file_operations proc_ops = {
     .read = proc_read;
 };
 
+/* This function is called when the module is loaded. */
 static int porc_init(void){
-    proc_create();
-    printk();
-    
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops); // creates the /proc/jiffies entry
     return 0;
 }
+
+/* This function is called when the module is removed. */
+static void proc_exit(){
+    remove_proc_entry(PROC_NAME, NULL); // removes the /proc/jiffies entry
+}
+
+/* This function is called each time /proc/jiffies is read */
+static ssize_t proc_read(struct file* file, char __user* usr_buf, size_t count, loff_t* pos){
+    char buffer[BUFFER_SIZE];
+    int rv = 0;
+    static int completed = 0;
+
+    if(completed){
+        completed = 0;
+        return 0;
+    }
+    
+    completed = 1;
+    rv = sprintf(buffer, "Current value of jiffies: %lu\n", jiffies);
+    raw_copy_to_user(usr_buf, buffer, rv);
+    
+    return rv;
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("");
+MODULE_DESCRIPTION("jiffies module");
+MODULE_AUTHOR("");
