8718653e1e8408291304516c1f5e755766721985
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-06 10:10:43 +0800

    Add files via upload

---
+++ ch2/simple/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo insmod simple.ko
+sleep 3
+cat /proc/simple
+sleep 3
+cat /proc/simple
+sudo rmmod simple
+sudo dmesg -c
--- ch2/simple/simple.c
+++ ch2/simple/simple.c
@@ -4,19 +4,19 @@
 
 /* This function is called when the module is loaded. */
 int simple_init(void){
-    printk(KERN INFO "Loading Kernel Module∖n");
+    printk(KERN_INFO "Loading Kernel Module∖n");
     return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void){
-    printk(KERN INFO "Removing Kernel Module∖n");
+    printk(KERN_INFO "Removing Kernel Module∖n");
 }
 
 /* Macros for registering module entry and exit points. */
 module_init(simple_init);
 module_exit(simple_exit);
 
-MODULE LICENSE("GPL");
-MODULE DESCRIPTION("Simple Module");
-MODULE AUTHOR("tjyu");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("tjyu");
