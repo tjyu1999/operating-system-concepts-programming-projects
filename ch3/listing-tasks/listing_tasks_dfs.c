d81f802d701ead8459be7cf8d52c24a477a73c58
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-19 21:55:34 +0800

    Update listing_tasks_dfs.c

--- ch3/listing-tasks/listing_tasks_dfs.c
+++ ch3/listing-tasks/listing_tasks_dfs.c
@@ -10,19 +10,19 @@ void dfs(struct task_struct *init_task){
     list_for_each(list, &init_task->children){
         /* task points to the next child in the list */
         task = list_entry(list, struct task_struct, sibling);
-        printk(KERN_INFO "pid: %d\t pname: %ld\t state: %d\n", task->pid, task->comm, task->state);
+        printk(KERN_INFO "pid: %d\t pname: %ld\t state: %d\n", task->pid, task->comm, task->__state);
     }
 }
 
 int proc_init(void){
-    printk(KERN_INFO "Loading Kernel Module∖n");
+    printk(KERN_INFO "Loading Kernel Module\n");
     dfs(&init_task);
     
     return 0;
 }
 
 void proc_exit(void){
-    printk(KERN_INFO "Removing Kernel Module∖n");
+    printk(KERN_INFO "Removing Kernel Module\n");
 }
 
 module_init(proc_init);
