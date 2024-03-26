902a95c4afb1f15590f99902506a264ca97c9fd1
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-26 21:39:22 +0800

    Update simple.c

--- ch2/simple/simple.c
+++ ch2/simple/simple.c
@@ -4,13 +4,13 @@
 
 /* This function is called when the module is loaded. */
 int simple_init(void){
-    printk(KERN_INFO "Loading Kernel Module\n");
+    printk(KERN_INFO "Loading kernel module\n");
     return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void){
-    printk(KERN_INFO "Removing Kernel Module\n");
+    printk(KERN_INFO "Removing kernel module\n");
 }
 
 /* Macros for registering module entry and exit points. */
